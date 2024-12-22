4ace2045d4820c24c3660f26a7c38003c3a87aec
marko <markoffka.kuznetsov@mail.ru>
2024-12-22 16:36:10 +0700

    wtwrgehfgfdfdgfhmcgj

--- src/main/java/org/example/dem/ChatClient.java
+++ src/main/java/org/example/dem/ChatClient.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
+
 public class ChatClient {
     private static final String SERVER_ADDRESS = "localhost";
     private static final int SERVER_PORT = 12345;
--- src/main/java/org/example/dem/ChatServer.java
+++ src/main/java/org/example/dem/ChatServer.java
@@ -7,6 +7,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+
 public class ChatServer {
     private static final int PORT = 12345;
     private static List<ClientHandler> clients = new ArrayList<>();
